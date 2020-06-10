9ed6f6771a88065759477f8a488e49fa164359ed
mattostraszewski <mrostraszewski@gmail.com>
2020-06-10 00:29:00 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,129 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isMobileMenuOpen: false,
+    }
+  }
+
+  toggleMobileMenu = () => {
+    this.setState({
+      isMobileMenuOpen: !this.state.isMobileMenuOpen,
+    })
+  }
+
+
+  render() {
+
+
+    return (
+      <div className="App">
+        {/* <style>
+          a:hover {
+            color: yellow;
+            }
+        </style> */}
+
+        <div className='nav-bar'>
+
+          <div className="start-logo"></div>
+          {/* <div className='start-logo'>Start Bootstrap</div> */}
+
+          <ul className='nav-bar-tasks'>
+
+            <li className='nav-task'>
+              <a className='nav-link'>Services</a>
+            </li>
+
+            <li className='nav-task'>
+              <a className='nav-link'>Portfolio</a>
+            </li>
+
+            <li className='nav-task'>
+              <a className='nav-link'>About</a>
+            </li>
+
+            <li className='nav-task'>
+              <a className='nav-link'>Team</a>
+            </li>
+
+            <li className='nav-task'>
+              <a className='nav-link'>Contact</a>
+            </li>
+
+          </ul>
+
+          <nav className="mobile-nav">
+
+            <button className="hamburger"
+              onClick={this.toggleMobileMenu}>MENU</button>
+
+            {this.state.isMobileMenuOpen && <ul
+              className={`mobile-nav-bar-tasks ${
+                this.state.isMobileMenuOpen ? 'mobile-nav-list-open' : null
+                }`}>
+
+              <li className='mobile-nav-task'>
+                <a href='link' className='mobile-nav-link'>Services</a>
+              </li>
+
+              <li className='mobile-nav-task'>
+                <a href='link' className='mobile-nav-link'>Portfolio</a>
+              </li>
+
+              <li className='mobile-nav-task'>
+                <a href='link' className='mobile-nav-link'>About</a>
+              </li>
+
+              <li className='mobile-nav-task'>
+                <a href='link' className='mobile-nav-link'>Team</a>
+              </li>
+
+              <li className='mobile-nav-task'>
+                <a href='link' className='mobile-nav-link'>Contact</a>
+
+              </li>
+
+            </ul>}
+
+          </nav>
+
+        </div>
+
+        <div className="heroheader">
+
+          <div className='welcome-mess'>
+
+            <h1 className='welcome'>Welcome To Our Studio!</h1>
+
+          </div>
+
+          <div className='greeting'>
+
+            <h1 className='nice'>IT IS NICE TO MEET YOU</h1>
+
+          </div>
+
+          <div>
+
+            <button className='tell'>TELL ME MORE</button>
+
+          </div>
+
+        </div>
+
+        <div className='body1'>
+
+          <div></div>
+
+        </div>
+
+      </div>
+    )
+  }
+}
